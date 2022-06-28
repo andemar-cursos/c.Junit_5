408d92ce3d0cd58079984cfb454555b7c494da77
Anderson <andemartinezdev@gmail.com>
2022-06-27 22:12:17 -0500

    44 -> When & thenThrow
    - Se realizaron comprobaciones de excepciones con el thenThrow.
    - Para los test se debe de indicar que tipo de excepcion se espera recibir.

--- mockito_app/mockito/src/test/java/org/andemar/appmockito/ejemplos/services/Datos.java
+++ mockito_app/mockito/src/test/java/org/andemar/appmockito/ejemplos/services/Datos.java
@@ -13,6 +13,12 @@ public class Datos {
             new Examen(7L, "Ciencias")
     );
 
+    public final static List<Examen> EXAMENES_ID_NULL = Arrays.asList(
+            new Examen(null, "Matematicas"),
+            new Examen(null, "Lenguaje"),
+            new Examen(null, "Ciencias")
+    );
+
     public final static List<String> PREGUNTAS = Arrays.asList(
             "Aritmetica",
             "integrales",
