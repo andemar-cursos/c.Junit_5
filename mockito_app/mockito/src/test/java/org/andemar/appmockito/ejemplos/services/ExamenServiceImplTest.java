c4882820ec671c8a39c4088ab73388115157d7a7
Anderson <andemartinezdev@gmail.com>
2022-07-03 17:12:42 -0500

    51. Spy & doReturn
    - Se creo un test el cual se uso los spy, el cual ejecuta la implementacion real de los metodos.
    - Si se quiere hacer un hibrido entre spy y mocks, se recomienda que en los mocks la respuesta sea mediante el doAnswer o doReturn, debido que el when executa el metodo original al momento de la creacion del Spy.

--- mockito_app/mockito/src/test/java/org/andemar/appmockito/ejemplos/services/ExamenServiceImplTest.java
+++ mockito_app/mockito/src/test/java/org/andemar/appmockito/ejemplos/services/ExamenServiceImplTest.java
@@ -257,4 +257,31 @@ class ExamenServiceImplTest {
         assertEquals(5L, examen.getId());
         assertEquals("Matematicas", examen.getNombre());
     }
+
+    @Test
+    void testSpy() {
+//        ExamenRepository examenRepository = mock(ExamenRepository.class);
+        ExamenRepository examenRepository = spy(ExamenRepositoryImpl.class);
+        PreguntaRepository preguntaRepository = spy(PreguntaRepositoryImpl.class);
+        ExamenService examenService = new ExamenServiceImpl(examenRepository, preguntaRepository);
+
+        // 1. Con Mock pero se ejecuta el metodo al "almacenar" el when.
+//        when(preguntaRepository.findPreguntasPorExamenId(anyLong())).thenReturn(Datos.PREGUNTAS);
+        // 2. Se hace con doAnswer si se quiere agregar alguna logica dentro del retorno
+//        doAnswer(invocation -> {
+//            return Datos.PREGUNTAS;
+//        }).when(preguntaRepository).findPreguntasPorExamenId(anyLong());
+        // 3. Como los datos a retornar ya estan listo, con doReturn se indican y listo
+        doReturn(Datos.PREGUNTAS).when(preguntaRepository).findPreguntasPorExamenId(anyLong());
+
+
+        Examen examen = examenService.findExamenPorNombreConPreguntas("Matematicas");
+        assertEquals(5, examen.getId());
+        assertEquals("Matematicas", examen.getNombre());
+        assertEquals(5, examen.getPreguntas().size());
+        assertTrue(examen.getPreguntas().contains("Aritmetica"));
+
+        verify(examenRepository).findAll();
+        verify(preguntaRepository).findPreguntasPorExamenId(anyLong());
+    }
 }
