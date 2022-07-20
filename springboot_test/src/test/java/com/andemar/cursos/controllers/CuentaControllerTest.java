2d6ec2d2a7a7e1db96b477706c196aef09c72d87
Anderson <andemartinezdev@gmail.com>
2022-07-19 21:18:54 -0500

    79 -> @WebMvcTest y MockMvc
    - Se creo la clase CuentaControllerTest la cual nos permite realizar pruebas al controlador.
    
    - Las pruebas simulas un request, y se evalua diferentes resultados, como el tipo del contenido y algunos datos al interior de la respuesta.

---
+++ springboot_test/src/test/java/com/andemar/cursos/controllers/CuentaControllerTest.java
@@ -0,0 +1,43 @@
+package com.andemar.cursos.controllers;
+
+import com.andemar.cursos.services.CuentaService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static com.andemar.cursos.data.Datos.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(CuentaController.class)
+class CuentaControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private CuentaService cuentaService;
+
+    @Test
+    void testDetalle() throws Exception {
+        // Given
+        when(cuentaService.findById(1L)).thenReturn(crearCuenta001().orElseThrow());
+
+        // When
+        mvc.perform(get("/api/cuentas/1").contentType(MediaType.APPLICATION_JSON))
+                // Then
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.persona").value("Andemar"))
+                .andExpect(jsonPath("$.saldo").value("1000"));
+
+        verify(cuentaService).findById(1L);
+    }
+
+
+}
